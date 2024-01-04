2f8d2fa4ce7e2ba13129a1f00ce344b145462121
VEOveneht <109220128+VEOveneht@users.noreply.github.com>
2024-01-04 19:49:22 +0700

    Update PlayerMovement.cs
    - Perubahan Besar Pada Movement, Untuk Mengikuti Arah Kamera
    
    Tambahkan referensi ke Transform kamera (playerCamera) di Inspector Unity, dan seret atau berikan game object kamera ke slot yang telah disediakan pada skrip ini. Dengan cara ini, gerakan pemain akan mengikuti rotasi kamera ketika bergerak.

--- Script/PlayerMovement.cs
+++ Script/PlayerMovement.cs
@@ -1,20 +1,18 @@
+using UnityEditor.UIElements;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-
-    // Showing Selection to Unity
     [SerializeField] float movement = 5f;
     [SerializeField] float jump = 4f;
     [SerializeField] float sprint = 3f;
-    
     [SerializeField] LayerMask ground;
     [SerializeField] Transform groundCheck;
-    
+    [SerializeField] Transform playerCamera; // Tambahkan referensi ke Transform kamera
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,28 +22,89 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        
-        if (Input.GetKey(KeyCode.LeftShift) && verticalInput > 0) // Walk and Run
+
+        // Mendapatkan vektor arah gerakan berdasarkan rotasi kamera
+        Vector3 forward = playerCamera.forward;
+        Vector3 right = playerCamera.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * verticalInput + right * horizontalInput;
+
+        // Mengatur kecepatan bergerak dengan sprint
+        float speed = movement;
+        if (Input.GetKey(KeyCode.LeftShift) && verticalInput > 0)
         {
-            rb.velocity = new Vector3(horizontalInput * movement * sprint, rb.velocity.y, verticalInput * movement * sprint);
+            speed *= sprint;
         }
-        else // Just Walk
-        {
-            rb.velocity = new Vector3(horizontalInput * movement, rb.velocity.y, verticalInput * movement);
-        }
-        
-        if (Input.GetButtonDown("Jump") && IsGrounded()) // It's for Jump
+
+        // Mengatur kecepatan gerakan pemain berdasarkan arah yang dihadapkan oleh kamera
+        rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
+
+        // Melakukan lompatan jika di tanah
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
         }
     }
 
-    // True or False, When Jump Just Jump Once / Jump when You In The Ground
     bool IsGrounded()
     {
         return Physics.CheckSphere(groundCheck.position, .1f, ground);
     }
 }
+
+
+
+
+
+// public class PlayerMovement : MonoBehaviour
+// {
+//     Rigidbody rb;
+
+//     // Showing Selection to Unity
+//     [SerializeField] float movement = 5f;
+//     [SerializeField] float jump = 4f;
+//     [SerializeField] float sprint = 3f;
+    
+//     [SerializeField] LayerMask ground;
+//     [SerializeField] Transform groundCheck;
+    
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // Movement
+//         float horizontalInput = Input.GetAxis("Horizontal");
+//         float verticalInput = Input.GetAxis("Vertical");
+        
+//         if (Input.GetKey(KeyCode.LeftShift) && verticalInput > 0) // Walk and Run
+//         {
+//             rb.velocity = new Vector3(horizontalInput * movement * sprint, rb.velocity.y, verticalInput * movement * sprint);
+//         }
+//         else // Just Walk
+//         {
+//             rb.velocity = new Vector3(horizontalInput * movement, rb.velocity.y, verticalInput * movement);
+//         }
+        
+//         if (Input.GetButtonDown("Jump") && IsGrounded()) // It's for Jump
+//         {
+//             rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.z);
+//         }
+//     }
+
+//     // True or False, When Jump Just Jump Once / Jump when You In The Ground
+//     bool IsGrounded()
+//     {
+//         return Physics.CheckSphere(groundCheck.position, .1f, ground);
+//     }
+// }
